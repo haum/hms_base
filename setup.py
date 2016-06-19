2d91382546ea0ab59d579f0b6cd924a1bb7a4c57
Romain Porte <microjoe@microjoe.org>
2016-06-19 11:28:59 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hms_base',
+    version='1.0',
+    packages=['hms_base', 'hms_base.tests'],
+    url='https://github.com/haum/hms_base',
+    license='MIT',
+    author='Romain Porte (microjoe)',
+    author_email='microjoe@microjoe.org',
+    description='Base package for HAUM micro-services'
+)
